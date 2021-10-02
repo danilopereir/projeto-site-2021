9a56bdaad62c49d98135b6aa2ace02b37c87361c
danilopereir <pdanilo205@gmail.com>
2021-10-02 00:07:29 +0000

    projeto site

---
+++ menu/src/App.js
@@ -0,0 +1,24 @@
+import './Home/home';
+import './App.css';
+
+function App() 
+
+<nav>
+  <div id="menu-contraido">
+      <input type="checkbox">
+      <span></span>
+      <ul>
+          <li><a href="./">Home</a></li>
+          <li><a href="cursos.jsx">Curso de Libras</a></li>
+          <li><a href="surdez.jsx">Surdez</a></li>
+          <li><a href="videos.jsx">Vídeos</a></li>
+          <li><a href="contato.jsx">Contato</a></li>
+          <li><a href="sobre.jsx">Sobre</a></li>
+      </ul>
+  </div>        
+</nav>
+
+  );
+}
+
+export default App;
